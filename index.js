90ca2bf5907fa6471ba2a90ccf7b6723fe8ef4f9
janki-nt <janki@ninjatechnolabs.com>
2023-09-22 18:43:22 +0530

    modified change for ec2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoDbConnect= require ('./config/dbConnect')
 const routes = require('./routes/routes');
 
 
-mongoDbConnect(process.env.DATABASE_URL);
+// mongoDbConnect(process.env.DATABASE_URL);
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,8 +15,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+app.use('/home',(req,res) =>{
+    res.write("Welcome to NODE EXPRESS application...!!!")
+})
 app.use('/', routes)
 
 app.listen(2000, () => {
-    console.log(`Server Started at 5000`)
+    console.log(`Server Started at 2000 `)
 })
